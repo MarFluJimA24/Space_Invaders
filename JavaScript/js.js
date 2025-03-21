6f07a7e3a51bec43ebe297398be25af9e2f4a61b
MarFluJimA24 <a24marflujim@inspedralbes.cat>
2025-03-21 19:34:27 +0100

    He añadido movimiento a la nave 2 usando las teclas de flechas direccionales izquierda y derecha. Tambien, al observar que las naves se podian salir de la pantalla, he delimitado el rango por el que se pueden mover las naves para así evitar futuros problemas.

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,14 +1,11 @@
-//creamos las variables de los objectos que usaremos
 var navePlayer1, navePlayer2;
 
-//Creamos los objetos
 function startGame() {
     myGameArea.start();
     navePlayer1 = new component(30, 30, "pink", 1000, 750);
     navePlayer2 = new component(30, 30, "green", 600, 750);
 }
 
-//con esta funcion hacemos la creación del canvas
 var myGameArea = {
     canvas: document.createElement("canvas"),
     start: function () {
@@ -24,40 +21,48 @@ var myGameArea = {
         window.addEventListener('keyup', function (e) {
             myGameArea.keys[e.keyCode] = (e.type == "keydown")
         })
-
     },
     clear: function () { 
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-//instanciamos los objetos
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.speedX = 0;
     this.x = x;
     this.y = y;
+    
     this.update = function () {
         context = myGameArea.context;
         context.fillStyle = color;
         context.fillRect(this.x, this.y, this.width, this.height);
     }
+
     this.newPos = function () {
         this.x += this.speedX;
+
+        // Limitar los bordes
+        if (this.x < 0) this.x = 0;
+        if (this.x + this.width > myGameArea.canvas.width) this.x = myGameArea.canvas.width - this.width;
     }
 }
+
 function updateGameArea() {
     myGameArea.clear();
+    
+    // Movimiento nave 1 (A y D)
     navePlayer1.speedX = 0;
-    if (myGameArea.keys && myGameArea.keys[65]){navePlayer1.speedX = -5;}
-    if (myGameArea.keys && myGameArea.keys[68]){navePlayer1.speedX = 5;}
+    if (myGameArea.keys && myGameArea.keys[65]){navePlayer1.speedX = -7;} // A
+    if (myGameArea.keys && myGameArea.keys[68]){navePlayer1.speedX = 7;}  // D
     navePlayer1.newPos();
     navePlayer1.update();
+
+    // Movimiento nave 2 (Flechas Izquierda y Derecha)
+    navePlayer2.speedX = 0;
+    if (myGameArea.keys && myGameArea.keys[37]){navePlayer2.speedX = -7;} // Flecha Izquierda
+    if (myGameArea.keys && myGameArea.keys[39]){navePlayer2.speedX = 7;}  // Flecha Derecha
+    navePlayer2.newPos();
+    navePlayer2.update();
 }
-
-
-
-
-
-
